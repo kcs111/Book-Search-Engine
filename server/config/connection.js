952e1057d66101b85b5f88faa62ab395a448e149
Kcs111 <kcs111@txstate.edu>
2021-12-02 10:55:12 -0600

    reslovers

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const root = require('app-root-path');
+const path = require('path');
+
+require('dotenv').config({path:root +path.sep + ".env"});
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
